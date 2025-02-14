148edab5aa276cf8007893401406851f6c158a0b
mathieudutour <mathieu@dutour.me>
2025-02-14 12:56:42 +0100

    Initial Commit

---
+++ FogMap/FogMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FogMapApp.swift
+//  FogMap
+//
+//  Created by Mathieu Dutour on 14/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct FogMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
